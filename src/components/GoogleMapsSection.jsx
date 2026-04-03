ca0faee729ff8c398bc33f06e30f7bbf745cf709
THIELCY BENDELO <bendelothielcy@gmail.com>
2026-04-03 17:20:49 +0100

    commentaire de correction et des tous les fichiers modifier

--- src/components/GoogleMapsSection.jsx
+++ src/components/GoogleMapsSection.jsx
@@ -4,7 +4,7 @@ function GoogleMapsSection() {
   return (
     <section className="py-12 px-4 bg-gradient-to-b from-black-50 via-black to-blue-50" id="localisation">
       <div className="max-w-3xl mx-auto text-center">
-        <h2 className="text-3xl font-extrabold text-blue-700 mb-4 drop-shadow-lg">Localisation professionnelle</h2>
+        <h2 className="text-3xl font-extrabold text-red-500 to-red-300 to-red-200 mb-4 drop-shadow-lg">Localisation professionnelle</h2>
         <p className="text-lg text-gray-400 mb-6">Retrouvez-moi à mon siège, Avenue Kimwenza A/, Kinshasa, DR Congo. Je suis disponibles pour tous vos projets professionnels.</p>
         <div className="rounded-2xl overflow-hidden shadow-xl font-black border border-blue-100">
           <iframe
